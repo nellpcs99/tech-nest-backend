e76c1fcbccaf029a67f70b3920bcb49766e750f1
nell pcs <nellpcs93@gmail.com>
2026-06-15 06:19:13 +0100

    Fix Pillow build on Render

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
+pip install --upgrade pip
+pip install pillow --no-cache-dir
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
