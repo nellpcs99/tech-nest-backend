14820bdb1d995751a149c4cb46b6451992e44b47
nell pcs <nellpcs93@gmail.com>
2026-06-15 06:35:29 +0100

    Fix collectstatic ignore map files

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 pip install --upgrade pip
-pip install pillow --no-cache-dir
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input --ignore=*.map
 python manage.py migrate
